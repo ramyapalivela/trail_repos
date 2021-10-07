a575e712110e726751daa49927e2f6312ea9a5b0
Admin <Admin@DESKTOP-F8V312A>
2021-10-07 12:15:06 +0530

    REQ-103 Added the bookService class to the project

---
+++ book-service/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Book;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private Book book;
+	public Book getBookById(int id) {
+		book.setBookId(201);
+		book.setBookName("Head First Java");
+		book.setAuthor("Kathy Sierra");
+		if(id ==1){
+			book.setBookId(202);
+			book.setBookName("Effective Java");
+			book.setAuthor("Bruce Erickel");
+		}
+		
+		return this.book;
+	}
+	
+	
+	
+
+}
