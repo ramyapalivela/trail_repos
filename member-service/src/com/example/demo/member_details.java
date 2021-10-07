d0aff0124f7ad3ea4390f8c10071b167b28666e3
Ramya <ramyapalivela123@gmail.com>
2021-10-07 14:39:41 +0530

    REQ-101 Printing Member details

---
+++ member-service/src/com/example/demo/member_details.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class member_details {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Member Name:= Ramya Palivela");
+		System.out.println("Member Place:= Andhra Pradesh");
+	}
+
+}
